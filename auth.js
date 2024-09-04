6e2bb55160d4f6f481098e20379feb2026be3b6e
Linwaru <linwaru@outlook.com>
2024-09-04 10:55:41 +0000

    deploy

--- auth.js
+++ auth.js
@@ -1,15 +1,14 @@
-// auth.js
-
 // Função para verificar a autenticação
 function checkAuthentication() {
     const isAuthenticated = localStorage.getItem('isAuthenticated');
     const userAvatar = localStorage.getItem('userAvatar');
     const loginPage = '/'; // Página de login
+    const protectedPages = ['/src/pages/form.html']; // Páginas protegidas
     const currentPage = window.location.pathname;
 
     // Verifica se o usuário está autenticado
     if (!isAuthenticated) {
-        if (currentPage !== loginPage) {
+        if (protectedPages.includes(currentPage)) {
             window.location.href = loginPage; // Redireciona para a página de login se não estiver autenticado
         }
     } else {
@@ -31,7 +30,7 @@ function displayUserAvatar(userAvatar) {
             // Logout: remove dados de autenticação e redireciona para a página de login
             localStorage.removeItem('isAuthenticated');
             localStorage.removeItem('userAvatar');
-            window.location.href = '/'; // Redireciona para a página inicial
+            window.location.href = '/'; // Redireciona para a página de login
         });
     }
 }
