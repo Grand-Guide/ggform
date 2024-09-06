837d682bb7174508ee4c9023a336ec959b8384b2
Linwaru <linwaru@outlook.com>
2024-09-06 02:25:47 +0000

    deploy

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-_app.default.listen(3000, () => console.log('Server started!'));
