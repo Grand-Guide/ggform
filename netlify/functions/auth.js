b651bcf58ee1f88249b3d7a6790d22d65ddf5c5c
Linwaru <linwaru@outlook.com>
2024-09-06 03:00:49 +0000

    deploy

---
+++ netlify/functions/auth.js
@@ -0,0 +1,14 @@
+// netlify/functions/auth.js
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = 'https://ggform.netlify.app/.netlify/functions/auth-callback';
+
+exports.handler = async function (event, context) {
+  const authorizationUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`;
+  
+  return {
+    statusCode: 302,
+    headers: {
+      Location: authorizationUrl,
+    },
+  };
+};
