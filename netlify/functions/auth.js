8e5ce874508c0bec016b228c4002c291a98bc6ef
Linwaru <linwaru@outlook.com>
2024-09-06 03:24:40 +0000

    deploy

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,8 +1,6 @@
 // netlify/functions/auth.js
-const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const REDIRECT_URI = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:8888/.netlify/functions/auth-callback'
-  : process.env.REDIRECT_URI;
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = 'https://ggform.netlify.app/.netlify/functions/auth-callback';
 
 exports.handler = async function (event, context) {
   const authorizationUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`;
