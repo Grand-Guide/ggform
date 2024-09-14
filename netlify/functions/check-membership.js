4e6a6f5dc9d855ca1513ed0a733376f99d901a21
Linwaru <linwaru@outlook.com>
2024-09-14 17:51:09 +0000

    deploy

--- netlify/functions/check-membership.js
+++ netlify/functions/check-membership.js
@@ -4,7 +4,7 @@ exports.handler = async function(event) {
     const { user_id, access_token } = JSON.parse(event.body);
 
     const guildId = '819380036351688716';
-    const discordApiUrl = `https://discord.com/api/users/@me/guilds`;
+    const discordApiUrl = `https://discord.com/api/v10/users/@me/guilds`;
 
     try {
         // Faz a requisição para pegar a lista de guildas do usuário
@@ -15,7 +15,17 @@ exports.handler = async function(event) {
             }
         });
 
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error('Erro ao obter guildas:', errorText);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Erro ao obter guildas do usuário' })
+            };
+        }
+
         const guilds = await response.json();
+        console.log('Guildas do usuário:', guilds); // Adicione esta linha para depuração
 
         // Verifica se o usuário faz parte do seu servidor específico
         const isMember = guilds.some(guild => guild.id === guildId);
@@ -33,6 +43,7 @@ exports.handler = async function(event) {
         }
 
     } catch (error) {
+        console.error('Erro ao verificar o servidor:', error.message); // Adicione esta linha para depuração
         return {
             statusCode: 500, // Erro interno do servidor
             body: JSON.stringify({ message: 'Erro ao verificar o servidor', error: error.message })
