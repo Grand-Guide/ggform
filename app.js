05d6ce0ec53306be0f6ba8d2eba4c4860e845688
KasynBR <thaliaredof@gmail.com>
2024-09-22 23:44:57 +0000

    deploy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.getElementById("login-btn").addEventListener("click", function () {
-    const clientId = process.env.DISCORD_APP;
+    const clientId = '1287177684006473818';
     const redirectUri = 'https://ggform.netlify.app/call-back';
     const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify%20guilds`;
 
     window.location.href = discordAuthUrl;
 });
 
-document.getElementById("server-btn").addEventListener("click", function () {
+document.getElementById("discord-btn").addEventListener("click", function () {
     const serverInviteUrl = 'https://discord.gg/GQx5MpX7cA';
 
     window.location.href = serverInviteUrl;
