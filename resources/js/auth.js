7ba632705cb79c8dcd4e487b4f24aef015b3e21e
KasynBR <thaliaredof@gmail.com>
2024-09-26 00:49:03 +0000

    deploy

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,4 +1,6 @@
-const { createClient } = supabase;
+import { createClient } from '@supabase/supabase-js';
+import 'dotenv/config';
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
