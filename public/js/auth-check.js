0ee8be226895bbdbc8e33223ea72347f3fe1ba44
KasynBR <thaliaredof@gmail.com>
2024-09-29 05:03:12 +0000

    deploy

--- public/js/auth-check.js
+++ public/js/auth-check.js
@@ -1,11 +1,25 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    const supabaseUrl = process.env.SUPABASE_URL;
-    const supabaseKey = process.env.SUPABASE_ANON_KEY;
-    const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-    
-    const { data: { user }, error } = await supabase.auth.getUser();
-    
-    if (!user) {
+// auth-check.js
+window.onload = async function() {
+    try {
+        const response = await fetch('/.netlify/functions/check-session', {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            if (response.status === 403) {
+                alert('Você está banido. Redirecionando para a página inicial.');
+            } else {
+                alert('Você não está logado. Redirecionando para a página inicial.');
+            }
+            window.location.href = '/';
+        }
+
+        const user = await response.json();
+        console.log('Usuário autenticado:', user);
+
+    } catch (error) {
+        console.error('Erro ao verificar sessão:', error);
         window.location.href = '/';
     }
-});
+}
